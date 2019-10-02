5d4a1e936dc9b830737c9788c45fc8dcd89e0811
elliottdehnbostel <dehnbostele@gmail.com>
2019-10-01 21:48:24 -0500

    fixed a bug rewarding multiple times within the reaction window

--- lib/transcribers/impl/RewardPlayerTranscriber.dart
+++ lib/transcribers/impl/RewardPlayerTranscriber.dart
@@ -4,11 +4,13 @@ import 'package:impulse/transcribers/Transcriber.dart';
 class RewardPlayerTranscriber extends Transcriber {
   @override
   writeToState() {
+    //this can also "do nothing", since the dichotomy is reward vs hurt
     String symbol = manager.getStateValue(AppStateKey.SYMBOL);
     List<String> killerSymbols =
         manager.getConfigValue(AppConfigKey.FAILURE_LETTERS);
     bool isKillerSymbol = killerSymbols.contains(symbol);
     bool isMissedWindow = manager.getStateValue(AppStateKey.REACTION_WINDOW_CLOSED);
+    int taps = manager.getStateValue(AppStateKey.SYMBOL_TAPPED_COUNT);
     if (isKillerSymbol) {
       int killerSymbolTotal = manager.getStateValue(AppStateKey.KILLER_SYMBOL_TOTAL);
       manager.updateState(AppStateKey.KILLER_SYMBOL_TOTAL, killerSymbolTotal + 1);
@@ -16,7 +18,7 @@ class RewardPlayerTranscriber extends Transcriber {
           manager.getStateValue(AppStateKey.KILLER_SYMBOL_STREAK);
       manager.updateState(
           AppStateKey.KILLER_SYMBOL_STREAK, killerSymbolStreak + 1);
-    } else if(!isMissedWindow){
+    } else if(!isMissedWindow && taps == 1){
       int successSymbolTotal = manager.getStateValue(AppStateKey.NORMAL_SYMBOL_TOTAL);
       manager.updateState(AppStateKey.NORMAL_SYMBOL_TOTAL, successSymbolTotal + 1);
       int normalSymbolStreak =
